8f9572589f893395c39a998aef55527da0d406f5
Gusta225 <g281231@dac.unicamp.br>
2025-01-29 14:23:38 -0300

    TOKEN test

--- st_app.py
+++ st_app.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-repo_url = f"https://{os.getenv('GITHUB_TOKEN')}@github.com/username/quanta_ml_interface.git"
-Repo.clone_from(repo_url, 'local_path')
+# repo_url = f"https://{os.getenv('GITHUB_TOKEN')}@github.com/DevpGus/Quanta_Ml_Interface.git"
+# Repo.clone_from(repo_url, 'local_path')
 
 
 def wide_mode():

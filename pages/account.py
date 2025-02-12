9429be627ad8a4028d58499098a9a4f3257d2fe7
Gusta225 <g281231@dac.unicamp.br>
2025-02-12 20:01:43 -0300

    login update

--- pages/account.py
+++ pages/account.py
@@ -6,6 +6,7 @@ import base64
 if 'authenticated' not in st.session_state:
     st.session_state.authenticated = False
 
+# Função para converter a imagem em base64 para adicioná-la ao HTML.
 def get_base64_of_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
@@ -15,9 +16,9 @@ with open('./assets/src/account.css') as f:
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
 # Login
-usernames = st.secrets["auth"]["usernames"]
-passwords = st.secrets["auth"]['passwords'] 
-name = st.secrets["auth"]['names']
+usernames = st.secrets["auth"]["usernames"] # Usuários cadastrados // Alterar em secrets.toml
+passwords = st.secrets["auth"]['passwords'] # Senhas cadastradas // Alterar em secrets.toml
+name = st.secrets["auth"]['names'] # Nomes cadastrados // Alterar em secrets.toml
 
 # Verificação de nome e senha com bcrypt.
 def verificar_senha(username, password):
@@ -39,64 +40,28 @@ def login():
                 st.session_state.authenticated = True
                 st.session_state.username = username
                 st.session_state.user_company = name[usernames.index(username)]
-                st.success("Login efetuado com sucesso! Redirecionando...")
+                st.toast("Login efetuado com sucesso! \nRedirecionando...", icon=":material/check_circle_outline:")
                 st.switch_page("pages/menu.py")
             else:
                 st.error("Usuário ou senha incorretos")
     return
 
-
-# USERS_DB = {
-#     "admin": [bcrypt.hashpw("123".encode(), bcrypt.gensalt()), "Quanta Júnior"],
-#     "user": [bcrypt.hashpw("user123".encode(), bcrypt.gensalt()), "Quanta Júnior"],
-# }
-
-# if 'authenticated' not in st.session_state:
-#     st.session_state.authenticated = False
-
-# def verificar_senha(username, password):
-#     if username in USERS_DB:
-#         if bcrypt.checkpw(password.encode(), USERS_DB[username][0]):
-#             return True
-#     return False
-
-# def login():
-#     st.title("Login")
-#     with st.container(border=True):
-#         username = st.text_input("Usuário")
-#         password = st.text_input("Senha", type="password")
-
-#         if st.button("Login", type="primary"):
-#             if verificar_senha(username, password):
-#                 st.session_state.authenticated = True
-#                 st.session_state.username = username
-#                 st.session_state.user_company = USERS_DB[username][1]
-#                 st.success("Login efetuado com sucesso! Redirecionando...")
-#                 st.switch_page("pages/menu.py")
-#             else:
-#                 st.error("Usuário ou senha incorretos")
-#     return
-
+# Verificação de autenticação
 if not st.session_state.authenticated:
     login()
 else:
 
-
-# Caminho da imagem local
+    # Caminho da imagem local
     image_path_1 = "./assets/src/img/perfil.png"
     image_path_2 = "./assets/src/img/help.png"
 
-# Convertendo a imagem para base64
+    # Convertendo a imagem para base64
     image_base64 = get_base64_of_image(image_path_1)
     image_base64_2 = get_base64_of_image(image_path_2)
 
 
-    # st.success(f"Logado como {st.session_state.username}")
     st.write("Aqui você pode acessar informações privadas.")    
-
-    with st.container():
-         
-            # st.image('./assets/src/img/perfil.png', width=100)
+    with st.container(): 
         st.html(f""" 
             <head>
             </head>    
@@ -149,8 +114,5 @@ else:
                 <br>
             </body>
         """)
-            # st.button("Perfil", key="perfil", type="secondary", use_container_width=True)
-            # st.button("Empresa", key="empresa", type="secondary", use_container_width=True)
-            # st.button("Docs", key="documentos", type="secondary", use_container_width=True)
 
 
